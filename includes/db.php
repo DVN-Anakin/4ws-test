bbdd3104c6ba69ba2f46c9dc22505f5cc41e071b
Pham The Anh <i.am.teralian@gmail.com>
2020-07-02 00:23:45 +0200

    heroku deploy db

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,9 @@
 <?php
-    $con = mysqli_connect("localhost","root","","4ws-test", "3307");
-    // $con = mysqli_connect("eu-cdbr-west-03.cleardb.net:3306","b78a198f7d2a64","2c0758f4","heroku_5a06a2225252fd9");
+    // Local dev
+    // $con = mysqli_connect("localhost","root","","4ws-test", "3307");
+
+    // Live
+    $con = mysqli_connect("eu-cdbr-west-03.cleardb.net:3306","bbcf89e61037f2","060ff5d9","heroku_1988942d393ae2e");
 
     if (mysqli_connect_errno()) {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
